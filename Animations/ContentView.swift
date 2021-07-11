295d4a1b5a27fb0a8c18a5abcdc4269907b11e58
Fray <dpineda@tribalworldwide.gt>
2021-07-11 12:32:12 -0400

    binding animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,31 +11,26 @@ struct ContentView: View {
     
     @State private var animationAmount: CGFloat = 1
     var body: some View {
-        Button("Tap me") {
-//            self.animationAmount += 1
+        
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
-        .padding(40)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(Color.red)
-                .scaleEffect(animationAmount)
-                .opacity(Double(2 - animationAmount))
-                .animation(
-                            Animation.easeInOut(duration: 1)
-                                .repeatForever(autoreverses: false)
-                        )
-        )
-        .onAppear(perform: {
-            self.animationAmount = 2
-        })
-//        .scaleEffect(animationAmount)
-//        .animation(
-//            Animation.easeInOut(duration: 1)
-//                .repeatForever()
-//        )
     }
 }
 
